67e4f7f40694add3f287a0f335890588d17a40cf
Guillermo Gravino <ggravino@gmail.com>
2023-03-10 09:26:40 -0300

    1 formulario + Templates funcionando

--- terceraentrega/urls.py
+++ terceraentrega/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from AppEstudiantes.views import Formulario_Estudiantes, Agregar_Estudiante
+from AppEstudiantes.views import Formulario_Estudiantes, Agregar_Estudiante, Principal
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('principal', Principal, name="pagina-principal"),
     path('gestionestudiantes', Formulario_Estudiantes, name="formulario-estudiantes"),
     path('gestionestudiantes/agregarestudiante', Agregar_Estudiante, name="agregar-estudiante"),
     
